7f26610245ea98cf05bf8970e9f8616d7b0f1af9
Kelvin Morrison <kelvinninja1@gmail.com>
2021-03-21 15:46:15 +0000

    Update env.php

--- config/env.php
+++ config/env.php
@@ -1,11 +1,5 @@
 <?php
     $dev_environment = true; //true //false
     $base_url = ($dev_environment) ? "http://localhost/booomers.club" : "";
-
-
-//    $environment = "dev"; //dev //test //beta //prod
-//    $base_url = ($environment == "http://localhost/booomers.club") ? "https://booomers.club" : "";
-//    $base_url = ($environment == "prod") ? "https://booomers.club" : (($environment == "dev") ? "http://localhost/booomers.club" : "https://beta.booomers.club");
-
-echo $base_url;
+//echo $base_url;
 ?>
